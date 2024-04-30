161fa5296e3df28f9d7a7622df73711f111e808d
Reinaldo <reinaldo.rossetti@outlook.com>
2024-04-30 11:41:12 -0300

    adicionando exemplo de for each reduzido

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -44,6 +44,7 @@ end
 Dado('os marcadores:') do |table|
   # Dado a string, faz um for each de caracteres, e inclue até encontrar o valor de pausa.
   # a regra do include? é usando o array caracteres_a_remover.
+  # for each reduzido ex: @text.chars.each { |char| @caracteres_a_remover.include?(char) ? break : @new_text << char }
   @caracteres_a_remover = table.raw.flatten
   @new_text = ""
   @text.chars.each { |char|
